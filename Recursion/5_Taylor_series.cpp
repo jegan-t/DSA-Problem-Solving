70cfa8935a428239461d2519e6b05040d7f32f08
Your Main Name <yourmain@email.com>
2025-10-01 21:56:01 +0530

    find exponential using recursion without static variable

--- Recursion/5_Taylor_series.cpp
+++ Recursion/5_Taylor_series.cpp
@@ -14,6 +14,28 @@ double exp(int x, int n = 10) {
 
 }
 
+/*
+with out static variable
+===============================================================
+#include <iostream>
+using namespace std;
+
+double expRec(int x, int n, double a = 1, double b = 1) {
+    if (n == 0) return 1;
+    double term = (a * x) / (b * n); // next term
+    return expRec(x, n - 1, a * x, b * n) + term;
+}
+
+int main() {
+    cout << expRec(1, 10) << endl;
+    return 0;
+}
+
+===========================================================
+
+
+ */
+
 int main() {
     cout << exp(1, 10) << endl;
     return 0;
